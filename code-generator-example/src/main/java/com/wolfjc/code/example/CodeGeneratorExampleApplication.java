47cedc057a92ace23e2332426b7a1c4d4612adc4
xudongdong <448280435@qq.com>
2018-07-10 18:29:54 +0800

    基本骨架

---
+++ code-generator-example/src/main/java/com/wolfjc/code/example/CodeGeneratorExampleApplication.java
@@ -0,0 +1,12 @@
+package com.wolfjc.code.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGeneratorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGeneratorExampleApplication.class, args);
+	}
+}
---
+++ code-generator-maven-plugin/src/main/java/com/wolfjc/code/generator/CodeGeneratorMojo.java
@@ -0,0 +1,23 @@
+package com.wolfjc.code.generator;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+
+/**
+ * @author xdd
+ * @date 2018/7/10.
+ */
+public class CodeGeneratorMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "application.properties")
+    private File configPath;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
