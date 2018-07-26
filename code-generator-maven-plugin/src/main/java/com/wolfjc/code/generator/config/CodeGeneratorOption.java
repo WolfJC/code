6e9de7075159ea3ec5fcc9e7a7b99578e6163301
xudongdong <448280435@qq.com>
2018-07-26 17:50:47 +0800

    去掉未使用的代码片段

--- code-generator-maven-plugin/src/main/java/com/wolfjc/code/generator/config/CodeGeneratorOption.java
+++ code-generator-maven-plugin/src/main/java/com/wolfjc/code/generator/config/CodeGeneratorOption.java
@@ -1,10 +1,6 @@
 package com.wolfjc.code.generator.config;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 /**
  * 生成模板代码时需要遵循的一些规则或者选项
@@ -76,34 +72,5 @@ public class CodeGeneratorOption {
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
-
-    /**
-     * 获取
-     * @return
-     */
-    public Collection<String> getTableNames() {
-        if (tableConfigs == null) {
-            return new ArrayList<>();
-        }
-        return tableConfigs.stream().map(TableConfig::getTableName).collect(Collectors.toList());
-    }
-
-
-    /**
-     * 根据表名查找对应的实体名
-     *
-     * @param tableName
-     * @return
-     */
-    public String findEntityName(String tableName) {
-        if (StringUtils.isEmpty(tableName)) {
-            return null;
-        }
-
-        return getTableConfigs().stream().filter(tableConfig -> tableConfig.getTableName().equals(tableName))
-                .map(TableConfig::getEntityName)
-                .findFirst().get();
-    }
-
 
 }
